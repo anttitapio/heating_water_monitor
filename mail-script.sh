24b0905812e3f3b8c3bf8e0e39b07534931b4ee8
Antti Pakarinen <antti.tapio.pakarinen@gmail.com>
2016-01-20 22:24:10 +0200

    Add e-mail recipient

--- mail-script.sh
+++ mail-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "Mittaustulos :" $1 |mail -s "Varaajan tuloveden lämpötila alhainen" antti.tapio.pakarinen@gmail.com
+echo "Mittaustulos :" $1 |mail -s "Varaajan tuloveden lämpötila alhainen" ossi.liukkonen@pp1.inet.fi
