e5ea02846818192a92860f017b349a215b5b41cc
Antti Pakarinen <antti.tapio.pakarinen@gmail.com>
2017-06-25 10:59:08 +0300

    fix email

--- mail-script.sh
+++ mail-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Mittaustulos :" $1 |mail -s "Varaajan tuloveden lämpötila alhainen" antti.tapio.pakarinen@gmail.com
-echo "Mittaustulos :" $1 |mail -s "Varaajan tuloveden lämpötila alhainen" ossi..pentti.liukkonen@gmail.com
+echo "Mittaustulos :" $1 |mail -s "Varaajan tuloveden lämpötila alhainen" ossi.pentti.liukkonen@gmail.com
