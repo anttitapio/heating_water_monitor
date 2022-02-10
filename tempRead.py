ecd9d2db859fbabc45b8037b3b8b2aa60f1bb701
Antti Pakarinen <antti.tapio.pakarinen@gmail.com>
2022-02-10 14:04:14 +0000

    Add logic for sending temp rising -mails

--- tempRead.py
+++ tempRead.py
@@ -7,18 +7,29 @@ import os
 import yagmail
 from subprocess import call
 from systemd import journal
+from collections import deque
+
+#class yag():
+#    def send(self, recipient, header, body):
+#        print("Sending mail to {}, header: {}, body: {}", recipient, header, body)
 
 SENSOR_CORRECTION_CONSTANT = 7.0 # align to analog meter
-ALARMING_LEVEL = 72.0
-MEASUREMENT_INTERVAL =  5 * 60 #seconds
+ALARMING_LEVEL = 75.0
+MEASUREMENT_INTERVAL =  6 * 60 #seconds
 OWN_DIR = os.path.dirname(os.path.realpath(__file__))
 TEMP_LOG_FILE = OWN_DIR + "/tempRead.log"
+TEMP_INIT_VALUE = 0
+ALLOWED_TEMP_DROP = 0.11
 APP_LOG_FILE = OWN_DIR + "/application.log"
+WEB_PAGE_FILE = OWN_DIR + "/index.html"
 MAIL_USER = os.environ['GM_USER']
 MAIL_PASS = os.environ['GM_PASS']
-MAIL_HEADER = 'Kattila sammunut: t='
+MAIL_HEADER_ALARM = 'Kattila sammunut, lämpötila laskussa: '
+MAIL_HEADER_OK = 'Kattila sytytetty, lämpotila nousussa: '
+MAIL_PREFIX = 'Viimeisin mittaukset: \n'
 MAIL_RECEIVER_FILE = OWN_DIR + "/mail_recipients.txt"
 yag = yagmail.SMTP(MAIL_USER, MAIL_PASS)
+logQue = deque(['']*10, maxlen=10)
 
 def init_sensor():
     #Check the serial number of the attached sensor
@@ -68,11 +79,14 @@ def get_date_and_time():
     return datetime.datetime.now().isoformat()
 
 
-def send_mail(tempAsString):
+def send_mail(tempAsString, mailHeader):
     recipients = read_mail_recipients_from_file()
+    mailbody = ''
+    for line in logQue:
+        mailbody += line
     for recipient in recipients:
         try:
-            yag.send(recipient, MAIL_HEADER + tempAsString, 'empty')
+            yag.send(recipient, mailHeader + tempAsString, MAIL_PREFIX + mailbody)
         except Exception as e:
             log("Error in sending mail: " + str(e))
         else:    
@@ -84,25 +98,60 @@ def log(log_entry, log_file = APP_LOG_FILE):
     logfile_entry = get_date_and_time() + ': ' + log_entry + '\n'
     logFile.write(logfile_entry)
     logFile.close()
-    journal.write(log_entry)
+    if log_file == TEMP_LOG_FILE:
+        logQue.appendleft(logfile_entry)
+        webFile = open(WEB_PAGE_FILE, "w")
+        for line in logQue:
+            webFile.write(line + '<br>')
+        webFile.close()
+    else:
+        journal.write(log_entry)
 
 
 def main_loop():
     consecutiveAlarms = 0
+    temperature = TEMP_INIT_VALUE
+    alarm = False
     while True:
+        previousTemperature = temperature
+        previousTempAsString = "%2.1f" % temperature
         temperature = read_temp()
         tempAsString = "%2.1f" % temperature
         log(tempAsString, TEMP_LOG_FILE)
         if temperature < ALARMING_LEVEL:
-            if (consecutiveAlarms % 10) == 0:
-                log("Low temperature read (" + tempAsString + "), sending mail")
-                send_mail(tempAsString)
-                consecutiveAlarms = 0
+            if temperature > previousTemperature:
+                if alarm:
+                    send_mail(tempAsString, MAIL_HEADER_OK)
+                    log("Temperature rising after alarm (" + tempAsString + "), "
+                        + " sending ok mail and seizing alarm")
+                elif previousTemperature != TEMP_INIT_VALUE:
+                    log("Temperature still low but rising, (" + previousTempAsString
+                        + " -> " + tempAsString + "), no new alarm")
+                alarm = False
+                continue
+
+            if (previousTemperature - temperature) > ALLOWED_TEMP_DROP:
+                if alarm == False:
+                    consecutiveAlarms = 0
+                alarm = True
+                log("Temperature falling (" + previousTempAsString + " -> "
+                    + tempAsString + "), setting alarm")
             else:
-                log("Temp value still low, waiting " + str(10 - consecutiveAlarms) \
+                log("Temperature low, no change (" + tempAsString + "), no new alarm")
+
+            if alarm and (consecutiveAlarms % 10) == 0:
+                log("Sending mail alarm")
+                send_mail(tempAsString, MAIL_HEADER_ALARM)
+                consecutiveAlarms = 0
+            elif alarm:
+                log("Alarm active, waiting "
+                    + str(10 - consecutiveAlarms)
                     + " rounds before sending a new mail")
-            consecutiveAlarms = consecutiveAlarms + 1
+            if alarm:
+                consecutiveAlarms += 1
         else:
+            log("Temperature at normal level (" + tempAsString + ")")
+            alarm = False
             consecutiveAlarms = 0
 
         time.sleep(MEASUREMENT_INTERVAL)
